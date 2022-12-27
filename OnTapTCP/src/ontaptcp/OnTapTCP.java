cb8bd9aed08f825bb1dfc20d99102c9b72b4d4da
Duc Bui <duc.bui@savvycomsoftware.com>
2022-12-28 01:42:13 +0700

    update

--- OnTapTCP/src/ontaptcp/OnTapTCP.java
+++ OnTapTCP/src/ontaptcp/OnTapTCP.java
@@ -5,7 +5,13 @@
  */
 package ontaptcp;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import model.Answer;
+import model.Student;
 
 /**
  *
@@ -19,7 +25,25 @@ public class OnTapTCP {
     public static void main(String[] args) {
         
         try{
-            Socket s = new Socket("192.168.0.2", 11310);
+            Socket s = new Socket("192.168.0.6", 11310);
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+            dos.writeUTF("B19DCCN186");
+            Student student = (Student) ois.readObject();
+            System.out.println(student.getMaSV());
+            student.setGroup(6);
+            student.setHovaten("Bui Minh Duc dep trai vl");
+            student.setIP("192.168.0.2");
+            
+            oos.writeObject(student);
+            oos.flush(); 
+            dos.writeUTF(student.getHovaten()); 
+            dos.writeInt(6); 
+            oos.writeObject(new Answer(student));
+            oos.close();
+            
             System.out.println("Server connected");
         }catch(Exception e){
             
